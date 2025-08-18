ae0ceac5ae26a1eb28edef06f1b537a972516717
IT24100644 <it24100644@my.sliit.lk>
2025-08-18 15:47:47 +0530

    Initial project files

---
+++ src/main/java/com/lankatex/garmentmanagement/LankaTexApplication.java
@@ -0,0 +1,13 @@
+package com.lankatex.garmentmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LankaTexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LankaTexApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lankatex/garmentmanagement/LankaTexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lankatex.garmentmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LankaTexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
