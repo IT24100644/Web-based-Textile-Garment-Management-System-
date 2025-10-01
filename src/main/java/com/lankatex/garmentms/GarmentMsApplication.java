2a4f6e514aef017f623cbee47f79200a72b0fc3d
IT24100600 <it24100600@my.sliit.lk>
2025-10-01 09:09:01 +0530

    Main application GarmentMs

---
+++ src/main/java/com/lankatex/garmentms/GarmentMsApplication.java
@@ -0,0 +1,13 @@
+package com.lankatex.garmentms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarmentMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GarmentMsApplication.class, args);
+	}
+
+}
